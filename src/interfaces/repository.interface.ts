d09b88b0418393701ad737ac799acfc4b368979e
leonardo.mendes <leodsmf@gmail.com>
2025-06-14 22:23:49 -0300

    definindo a tipagem

---
+++ src/interfaces/repository.interface.ts
@@ -0,0 +1,29 @@
+import { RepositoryCategory } from "../types/enums";
+
+export interface ICreateRepositoryRequestBody {
+  title: string;
+  date: string;
+  description: string;
+  linkDemo?: string;
+  linkGithub: string;
+  idIcon: number;
+  category: RepositoryCategory;
+  highlighted: boolean;
+}
+
+export interface IRepositoryAttributes {
+  id: string;
+  title: string;
+  description: string;
+  link_demo?: string;
+  link_github: string;
+  id_icon?: number;
+  category: RepositoryCategory;
+  highlighted: boolean;
+  CreatedAt: Date;
+  updatedAt?: Date;
+}
+
+// TODO: estudar o tipo Partial
+export type IUpdateRepositoryRequestBody =
+  Partial<ICreateRepositoryRequestBody>;
