98cf99ee236a46c128b19ed911a6843a978942b4
leonardo ferreira <leodsmf@gmail.com>
2025-07-18 22:54:40 -0300

    configurando as variaves de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from "express"
-import routes from "./routes";
+import 'dotenv/config'
+import express from 'express'
+import routes from './routes'
 import cors from 'cors'
-const app = express();
-const port = 3000;
+
+const app = express()
+const port = 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(routes);
+app.use(routes)
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
